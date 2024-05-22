b17bdc1bdb3c8767ea7a455d82ac7d2869717b32
stkmala <skmalah23@gmail.com>
2024-05-23 05:58:31 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        basis: '#f78b93',
+        basis: "#c49f64",
+        kedua: "#f0d6ad",
+        ketiga: "#f5e9d5",
+        keempat: "#f2ede6",
       },
       screens: {
-        '2xl': '1320px',
+        "2xl": "1320px",
+      },
+      backgroundImage: {
+        hero: "url('/src/img/bm.JPG')",
       },
     },
   },
   plugins: [],
-}
-
+};
